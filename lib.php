6824ad441a3c0c30ab667f5c19e947fc698b9554
Patrick Thibaudeau <thibaud@yorku.ca>
2024-07-02 13:14:34 -0400

    Added lib.php for file plugin support. Updated the version.

---
+++ lib.php
@@ -0,0 +1,35 @@
+<?php
+function block_ai_assistant_pluginfile($course, $cm, $context, $filearea, $args, $forcedownload, array $options = array())
+{
+    global $DB;
+
+    if ($context->contextlevel != CONTEXT_SYSTEM) {
+        return false;
+    }
+
+//    require_login(1, true);
+
+    $fileAreas = array(
+        'syllabus',
+    );
+
+    if (!in_array($filearea, $fileAreas)) {
+        return false;
+    }
+
+
+    $itemid = array_shift($args);
+    $filename = array_pop($args);
+    $path = !count($args) ? '/' : '/' . implode('/', $args) . '/';
+
+    $fs = get_file_storage();
+
+    $file = $fs->get_file($context->id, 'block_ai_assistant', $filearea, $itemid, $path, $filename);
+
+    // If the file does not exist.
+    if (!$file) {
+        send_file_not_found();
+    }
+
+    send_stored_file($file, 86400, 0, $forcedownload); // Options.
+}
