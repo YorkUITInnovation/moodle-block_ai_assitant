202411eb59e4495c95228d715a69567b98fd2cde
Kawshar <kwshr96626@gmail.com>
2024-07-08 12:11:27 -0400

    Updated default block settings

--- settings.php
+++ settings.php
@@ -29,11 +29,45 @@ if ($hassiteconfig) {
     $settings = new admin_settingpage('block_ai_assistant_settings', new lang_string('pluginname', 'block_ai_assistant'));
 
     //Bot Server
-    $settings->add( new admin_setting_configtext(
+    $settings->add(new admin_setting_configtext(
         'block_ai_assistant/cria_url',
         get_string('cria_url', 'block_ai_assistant'),
         get_string('cria_url_help', 'block_ai_assistant'),
-        '', PARAM_TEXT, 255
+        '',
+        PARAM_TEXT,
+        115
+    ));
+    $settings->add(new admin_setting_configtext(
+        'block_ai_assistant/cria_token',
+        get_string('cria_token', 'block_ai_assistant'),
+        get_string('cria_token_help', 'block_ai_assistant'),
+        '',
+        PARAM_TEXT,
+        10
+    ));
+    $settings->add(new admin_setting_configtext(
+        'block_ai_assistant/criadex_model_id',
+        get_string('criadex_model_id', 'block_ai_assistant'),
+        get_string('criadex_model_id_help', 'block_ai_assistant'),
+        '',
+        PARAM_TEXT,
+        10
+    ));
+    $settings->add(new admin_setting_configtext(
+        'block_ai_assistant/criadex_embed_id',
+        get_string('criadex_embed_id', 'block_ai_assistant'),
+        get_string('criadex_embed_id_help', 'block_ai_assistant'),
+        '',
+        PARAM_TEXT,
+        10
+    ));
+    $settings->add(new admin_setting_configtext(
+        'block_ai_assistant/criadex_rerank_id',
+        get_string('criadex_rerank_id', 'block_ai_assistant'),
+        get_string('criadex_rerank_id_help', 'block_ai_assistant'),
+        '',
+        PARAM_TEXT,
+        10
     ));
     // phpcs:ignore Generic.CodeAnalysis.EmptyStatement.DetectedIf
     if ($ADMIN->fulltree) {
