7cc6ed01264311a72b9d8de4f994cbc9510cc8fd
Kawshar <kwshr96626@gmail.com>
2024-07-15 12:54:58 -0400

    Added saving files to temp folder functionality

--- syllabus_upload.php
+++ syllabus_upload.php
@@ -35,9 +35,16 @@ $mform = new \block_ai_assistant\syllabus_upload_form(null, array('formdata' => 
 if ($mform->is_cancelled()) {
     //Handle form cancel operation, if cancel button is present on form
     redirect($CFG->wwwroot . '/course/view.php?id=' . $courseid);
-
 } else if ($data = $mform->get_data()) {
 
+    // Clear existing files in the draft area
+    $fs = get_file_storage();
+    $existingfiles = $fs->get_area_files($context->id, 'block_ai_assistant', 'syllabus', $data->courseid, 'itemid', false);
+    foreach ($existingfiles as $existingfile) {
+        $existingfile->delete();
+    }
+
+    //save the uploaded file in the draft area
     file_save_draft_area_files(
         $data->syllabus_upload,
         $context->id,
@@ -47,10 +54,22 @@ if ($mform->is_cancelled()) {
         array('subdirs' => 0, 'maxfiles' => 1)
     );
 
+    //get files from the draft area
+    $fs = get_file_storage();
+    $file = $fs->get_area_files($context->id, 'block_ai_assistant', 'syllabus', $courseid);
+
+    //save it to moodledata/tmp/{courseid}/cria folder
+    if ($file) {
+        $file = reset($file);
+        $temppath = $CFG->dataroot . 'temp/' . $courseid . '/cria';
+        if ($$temppath) {
+            mkdir($temppath, 0777, true);
+            $file->copy_content_to($temppath . '/' . $file->get_filename());
+        }
+    }
+
     // Redirect with success message
     redirect($CFG->wwwroot . '/course/view.php?id=' . $courseid, get_string('file_uploaded_successfully', 'block_ai_assistant'), null, \core\output\notification::NOTIFY_SUCCESS);
-
-
 } else {
     // Show form
     $mform->set_data($formdata);
