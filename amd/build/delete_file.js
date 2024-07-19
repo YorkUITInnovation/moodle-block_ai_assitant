28c9bfc9f74ec2827b8182bb0e14d7d9552674f7
Kawshar <kwshr96626@gmail.com>
2024-07-19 11:13:41 -0400

    Add latest build artifacts and ignore grunt config

---
+++ amd/build/delete_file.js
@@ -0,0 +1,36 @@
+import notification from 'core/notification';
+import ajax from 'core/ajax';
+import * as Str from 'core/str';
+
+export const init = () => {
+    enable_ai_assitant();
+};
+
+/**
+ * Delete a content
+ */
+function delete_syllabus() {
+    document.getElementById('btn-ai-assistant-delete-syllabus').addEventListener('click', function () {
+        // get data-courseid from current element
+        var courseid = this.getAttribute('data-courseid');
+        // Pop up notificaiton to confirm delete
+        notification.confirm(Str.get_string('delete', 'block_ai_assistant'),
+            Str.get_string('delete_syllabus_help', 'block_ai_assistant'),
+            Str.get_string('delete', 'block_ai_assistant'),
+            Str.get_string('cancel', 'block_ai_assistant'), function () {
+                //Delete the record
+                var delete_content = ajax.call([{
+                    methodname: 'ai_assistant_delete_syllabus',
+                    args: {
+                        'courseid': courseid
+                    }
+                }]);
+
+                delete_content[0].done(function () {
+                    location.reload();
+                }).fail(function () {
+                    alert('An error has occurred. The record was not deleted');
+                });
+            });
+    });
+}
