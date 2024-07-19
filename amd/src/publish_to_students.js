bbc83a04a447c9bc0a28099dcbb8ef259457ec14
Kawshar <kwshr96626@gmail.com>
2024-07-19 10:39:54 -0400

    Fixed publishing callback function

--- amd/src/publish_to_students.js
+++ amd/src/publish_to_students.js
@@ -16,14 +16,14 @@ function enable_ai_assitant() {
         var courseid = this.getAttribute('data-courseid');
         // Pop up notificaiton to confirm delete
 
-        //Delete the record
+        //Publish the record
         var enable_content = ajax.call([{
             methodname: 'ai_assistant_publish',
             args: {
                 'courseid': courseid
             }
         }]);
-        delete_content[0].done(function () {
+        enable_content[0].done(function () {
             location.reload();
         }).fail(function () {
             alert('An error has occurred. The record was not published');
