eb73ff347f2e138e12c766c4f291f752c8b575c4
Patrick Thibaudeau <thibaud@yorku.ca>
2024-07-31 06:42:36 -0400

    Built array for course structure

--- tmp_files/modtest.php
+++ tmp_files/modtest.php
@@ -22,40 +22,71 @@ $PAGE->set_heading('Test');
 $config = get_config('block_ai_assistant');
 echo $OUTPUT->header();
 
+$course_structure = new stdClass();
+// Get accepted modules
 $accepted_modules = explode(',', $config->accepted_modules);
-
-
+// Get course modules
 $modules = get_fast_modinfo($courseid);
 // Get all sections from $modules and print them.
 $sections = $modules->get_section_info_all();
+// Loop through sections
+$i = 0; // Used to count the number of sections
 foreach ($sections as $sectionnum => $section) {
-    echo '<h2>' . $section->name . '</h2>';
-    if(isset($modules->sections[$section->section])) {
+    $course_structure->sections[$i] = new stdClass();
+    $course_structure->sections[$i]->sectionnum = $sectionnum;
+    $course_structure->sections[$i]->name = $section->name;
+//    echo '<h2>' . $section->name . '</h2>';
+    $x = 0; // Used to count the number of modules in a section
+    if (isset($modules->sections[$section->section])) {
         $section_mods = $modules->sections[$section->section];
         foreach ($section_mods as $cmid) {
             $mod = get_module_from_cmid($cmid);
             if (in_array($mod[1]->modname, $accepted_modules)) {
-                echo '<h5>' . $mod[0]->name . '</h5>';
+                $course_structure->sections[$i]->modules[$x] = new stdClass();
+                $course_structure->sections[$i]->modules[$x]->name = $mod[0]->name;
+                $course_structure->sections[$i]->modules[$x]->intro = $mod[0]->intro;
+                $course_structure->sections[$i]->modules[$x]->instanceid = $mod[0]->id;
+                $course_structure->sections[$i]->modules[$x]->cmid = $mod[1]->id;
+                // Prepare the content based on the type of module
+                switch ($mod[1]->modname) {
+                    case 'page':
+                        $course_structure->sections[$i]->modules[$x]->content = $mod[0]->content;
+                        break;
+                    case 'label':
+                        $course_structure->sections[$i]->modules[$x]->content = $mod[0]->intro;
+                        break;
+                    case 'book':
+                        // Must get book content
+                        break;
+                    case 'resource': // File
+                        // Index the file
+                        break;
+                    case 'folder':
+                        // Index the folder files
+                        break;
+                }
+//                echo '<h5>' . $mod[0]->name . '</h5>';
 
+
+                $x++;
 //                print_object($mod);
             }
         }
     }
-
+    $i++;
 }
 
-
-
-
-function get_module_from_cmid($cmid) {
+print_object($course_structure);
+function get_module_from_cmid($cmid)
+{
     global $CFG, $DB;
     if (!$cmrec = $DB->get_record_sql("SELECT cm.*, md.name as modname
                                FROM {course_modules} cm,
                                     {modules} md
                                WHERE cm.id = ? AND
-                                     md.id = cm.module", array($cmid))){
+                                     md.id = cm.module", array($cmid))) {
         throw new \moodle_exception('invalidcoursemodule');
-    } elseif (!$modrec =$DB->get_record($cmrec->modname, array('id' => $cmrec->instance))) {
+    } elseif (!$modrec = $DB->get_record($cmrec->modname, array('id' => $cmrec->instance))) {
         throw new \moodle_exception('invalidcoursemodule');
     }
     $modrec->instance = $modrec->id;
@@ -64,7 +95,6 @@ function get_module_from_cmid($cmid) {
 
     return array($modrec, $cmrec);
 }
-
 
 
 echo $OUTPUT->footer();
