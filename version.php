240aad11e7b68591763e8fe64ffe38e0c671a9a3
Patrick Thibaudeau <thibaud@yorku.ca>
2025-08-04 12:55:10 -0400

    Updated version

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'block_ai_assistant';
-$plugin->release = '2.0.1 (Build: 2025080300)';
+$plugin->release = '2.0.0 (Build: 2025080300)';
 $plugin->version = 2025080300;
 $plugin->requires = 2022041900;
 $plugin->maturity = MATURITY_STABLE;
