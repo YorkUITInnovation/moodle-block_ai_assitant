feec49989cc3fe8bdc7a3dd1720abd95dbe5ec99
Patrick Thibaudeau <thibaud@yorku.ca>
2026-03-12 19:52:56 -0400

    fix: correct chat webservice start method mapping and return definition
    - remove trailing space from the `block_ai_assistant_chat_start` service
      method name in `db/services.php`
    - fix `block_ai_assistant_chat_ws::start_returns()` to return the proper
      external structure type
    - align the declared return structure with the actual payload returned by
      `block_ai_assistant_chat_ws::start()`
    
    This fixes Moodle webservice definition errors when loading the chat start
    service in the admin UI.

--- version.php
+++ version.php
@@ -26,6 +26,6 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'block_ai_assistant';
 $plugin->release = '2.0.1 (Build: 2025072906)';
-$plugin->version = 2025072906;
+$plugin->version = 2025072908;
 $plugin->requires = 2022041900;
 $plugin->maturity = MATURITY_STABLE;
